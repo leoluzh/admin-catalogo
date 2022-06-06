1427fd95f1eceb4000fd2e6b38f57b1428e71b62
leonardo luz fernandes <leonardo.l.fernandes@gmail.com>
2022-06-06 00:24:31 -0300

    Inicio da criacao do caso de uso de atulizacao de categoria.

---
+++ application/src/test/java/com/lambdasys/admim/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -0,0 +1,9 @@
+package com.lambdasys.admim.catalogo.application.category.update;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateCategoryUseCaseTest {
+
+}
