725de0c4163bcbd4c3ba5e49c9a90751e5eac3bd
leonardo luz fernandes <leonardo.l.fernandes@gmail.com>
2022-06-19 16:37:33 -0300

    Inclusao de mapper.

--- domain/src/main/java/com/lambdasys/admin/catalogo/domain/pagination/Pagination.java
+++ domain/src/main/java/com/lambdasys/admin/catalogo/domain/pagination/Pagination.java
@@ -1,8 +1,32 @@
 package com.lambdasys.admin.catalogo.domain.pagination;
 
+import java.util.List;
+import java.util.function.Function;
+
 public record Pagination<T>(
         int currentPage ,
         int perPage  ,
         long total ,
-        T itens ) {
+        List<T> items ) {
+
+
+    public static <T> Pagination<T> from( int currentPage , int perPage , long total , List<T> items){
+        return new Pagination<T>(
+                currentPage,
+                perPage,
+                total,
+                items);
+    }
+
+
+    public <R> Pagination<R> map(final Function<T,R> mapper){
+        final var mappedItems = this.items.stream().map(mapper).toList();
+        return new Pagination<>(
+                this.currentPage() ,
+                this.perPage() ,
+                this.total() ,
+                mappedItems
+        );
+    }
+
 }
