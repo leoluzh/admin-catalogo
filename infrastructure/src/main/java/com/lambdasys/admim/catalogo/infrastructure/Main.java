47a26cb851f74de7c27f2a1f4f30781c674c1b5b
leonardo luz fernandes <leonardo.l.fernandes@gmail.com>
2022-06-20 00:57:14 -0300

    Inclusao de configuração inicial de springboot.

--- infrastructure/src/main/java/com/lambdasys/admim/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/lambdasys/admim/catalogo/infrastructure/Main.java
@@ -1,10 +1,15 @@
 package com.lambdasys.admim.catalogo.infrastructure;
 
-import com.lambdasys.admim.catalogo.application.UseCase;
+import com.lambdasys.admim.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(new UseCase().execute());
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,"development");
+        SpringApplication.run(WebServerConfig.class,args);
     }
 }
---
+++ infrastructure/src/main/java/com/lambdasys/admim/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,13 @@
+package com.lambdasys.admim.catalogo.infrastructure.configuration;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lambdasys.admim.catalogo")
+public class WebServerConfig {
+
+
+
+}
